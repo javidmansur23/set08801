f43567c5fd7d13ee591bb0b6c7ffc3dce9b5106a
Javid Mansur <123555843+javidmansur23@users.noreply.github.com>
2023-04-27 13:57:34 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
+var mainListDiv = document.getElementById("mainListDiv"),
+    mediaButton = document.getElementById("mediaButton"),
+    navBar = document.querySelector("header .navbar"),
+    signup = document.querySelector(".signup");
+
+mediaButton.onclick = function () {
+    
+    "use strict";
+    
+    navBar.classList.toggle("show_list");
+    mediaButton.classList.toggle("active");
+    
+};
+
+navBar.onclick = function(){
+    navBar.classList.remove("show_list");
+}
+
 let header = document.querySelector('header');
 let menu = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-
 
 window.addEventListener( 'scroll', () => {
     header.classList.toggle('active', window.scrollY > 0);
 });
-
-menu.onclick = () => {
-    navbar.classList.toggle('active');
-}
-window.onscroll = () => {
-  navbar.classList.remove('active');
-}
